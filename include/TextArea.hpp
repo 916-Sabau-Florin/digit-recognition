b2f60e9a94301cde3c105ccee3a960d66540eebc
Florin-Vlad Sabau <sabauflorin28@gmail.com>
2020-10-11 15:22:15 +0300

    Added CMakeLists.txt

--- include/TextArea.hpp
+++ include/TextArea.hpp
@@ -7,14 +7,14 @@ class TextArea : public Widget
 {
 public:
     TextArea(const sf::FloatRect &rect, const std::string &str);
-    ~TextArea();
-    void handleEvent(sf::Event event);
+    ~TextArea() override;
+    void handleEvent(sf::Event event) override;
     void center();
-    void update();
+    void update() override;
     void setText(const std::string &str);
 
 protected:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 
 private:
--- include/Widget.hpp
+++ include/Widget.hpp
@@ -6,7 +6,7 @@ namespace gui {
 class Widget : public sf::Drawable
 {
 public:
-    virtual ~Widget() = 0;
+    ~Widget() override = 0;
     virtual void update() = 0;
     virtual void handleEvent(sf::Event event) = 0;
 protected:
